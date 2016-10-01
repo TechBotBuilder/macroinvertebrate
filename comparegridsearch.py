cfb0ceee9d14cfd398eda3c15bf36269a276c96d
Maxwell Budd <techbotbuilder@gmail.com>
2016-10-01 19:17:04 -0400

    Cleanup comparison tool

--- comparegridsearch.py
+++ comparegridsearch.py
@@ -4,12 +4,14 @@ import pickle
 import collections
 import operator
 
-VERSION = 'gridsearch'
+def error(msg):
+    
+    exit(2) #https://docs.python.org/2/library/sys.html#sys.exit -> 2=command line syntax errors
 
-def compare(sortby, num_output, output=False):
+def compare(version, sortby, num_output, output=False):
     #data like list of
     #(SUBVERSION, {'loss':#, 'val_loss':#, 'acc':#, 'val_acc':#})
-    with open("models/{}/results.pickle".format(VERSION), 'rb') as f:
+    with open("models/{}/results.pickle".format(version), 'rb') as f:
         results = pickle.load(f)
     if sortby == 'combo':
         val_accs = dict([dp for dp in enumerate([(d[1]['acc']*d[1]['val_acc'])**0.5 for d in results])])
@@ -20,26 +22,28 @@ def compare(sortby, num_output, output=False):
     if output:
         for dp in val_accs[0:min(len(val_accs), num_output)]:
             d = results[dp[0]]
-            print("{}:\t acc={:.3}, val_acc={:.3}".format(d[0], d[1]['acc'], d[1]['val_acc']))
+            print("{:45} acc={:.3}, val_acc={:.3}".format(d[0]+":", d[1]['acc'], d[1]['val_acc']))
 
 if __name__ == "__main__":
     sortbyops = ('combo', 'acc', 'val_acc')
     from sys import argv
     n = 5
     sortby = sortbyops[0]
-    if len(argv) > 1:
-        sortby = argv[1]
+    if len(argv) < 2:
+        error("Options:\n"
+        + "<model_type> [sort_by [num_results]]")
+    version = argv[1] + "_gridsearch"
+    if len(argv) > 2:
+        sortby = argv[2]
         if sortby not in sortbyops:
             msg = "Sort by one of:"
             for sb in sortbyops:
                 msg = msg + "  '"+sb+"'"
             msg = msg + "."
-            print(msg)
-            exit(2) #https://docs.python.org/2/library/sys.html#sys.exit -> 2=command line syntax errors
-        if len(argv) > 2:
-            try:
-                n = int(argv[2])
-            except:
-                print("Second argument must be an integer. Given", argv[2] + ".")
-                exit(2)
-    compare(sortby, n, True)
+            error(msg)
+    if len(argv) > 3:
+        try:
+            n = int(argv[3])
+        except:
+            error("Third argument must be an integer. Given", argv[3] + ".")
+    compare(version, sortby, n, True)
