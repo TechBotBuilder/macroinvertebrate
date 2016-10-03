6ee65c7428e75f61962a8d261e0840b428f18b02
Maxwell Budd <techbotbuilder@gmail.com>
2016-10-03 18:06:10 -0400

    Use command-line arguments

--- ff.py
+++ ff.py
@@ -1,9 +1,3 @@
-#Macroinvertebrate classification system
-#Using Artificial Neural Networks
-#this file for feedforward networks
-#By Maxwell Budd
-#techbotbuilder.com/neuralnet
-
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers import Reshape
@@ -15,16 +9,8 @@ from keras.models import model_from_json
 import os
 import pickle
 
-IMAGE_DIMENSION = 64
-BATCH_SIZE = 64
 NB_EPOCH = 100
 SAMPLES_PER_EPOCH = 2048
-
-VERSION = "ff_grid_1"
-SUBVERSION = 0
-
-NUM_HIDDEN_LAYERS = 2
-HIDDEN_SIZE = 2**10
 
 NUM_VALIDATION_SAMPLES = 113
 
@@ -38,25 +24,67 @@ def log(message):
     from datetime import datetime
     print("[{}] {}".format(str(datetime.now())[:19], message))
 
+def parse_subversion(subversion_descriptor):
+    sv=subversion_descriptor
+    def whereis(letter, after=0, suppress_error=False):
+        pos_letter = sv.find(letter, after)
+        return pos_letter
+        #if pos_letter==-1
+        #    if suppress_error: return False
+        #    else:
+        #       print("Invalid subversion string:\n"
+        #        + "Could not find {} in {}".format(letter, sv))
+        #        exit(2)
+        #else: return pos_letter
+    positions = {letter:whereis(letter) for letter in "HNIAL"}
+    results = {}
+    results['optimizer'] = sv[:positions['H']].lower()
+    results['hidden_size'] = int(sv[positions['H']+1:positions['N']-1])
+    results['batch_size'] = int(sv[positions['N']+1:positions['I']-1])
+    results['image_dimension'] = int(sv[positions['I']+1:positions['A']-1])
+    results['activation'] = sv[positions['A']+1:positions['L']-1]
+    if results['activation'] == 'sigm': results['activation'] = 'sigmoid'
+    results['loss'] = sv[positions['L']+1:positions['L']+5] #loss is 4 chars long
+    if results['loss'] == 'cate': results['loss'] = "categorical_crossentropy"
+    if results['loss'] == 'mean': results['loss'] = "mean_squared_error"
+    pos_l = whereis('l', positions['L'], True)
+    if pos_l != -1: results['learning_rate'] = 2**-int(sv[pos_l+1:pos_l+2])
+    pos_m = whereis('m', pos_l, True)
+    if pos_m != -1: results['momentum'] = float(sv[pos_m+1:pos_m+4])
+    pos_D = whereis('D', positions['L'])
+    results['deepness'] = int(sv[pos_D+1:pos_D+2])
+    return results
 
 if __name__ == "__main__":
     log("Starting program...")
-    if SUBVERSION=="0":
-        #make a (image input) -> (NUM_HIDDEN_LAYERS * )(hidden) -> (classification output) network
-        model = Sequential()
-        model.add(Reshape((IMAGE_DIMENSION**2,), input_shape=(1, IMAGE_DIMENSION, IMAGE_DIMENSION)))
-        for _ in range(NUM_HIDDEN_LAYERS):
-            model.add(Dense(HIDDEN_SIZE))
-            model.add(Activation('relu'))
-        model.add(Dense(NB_CLASSES))
-        model.add(Activation('softmax'))
-    else:
-        with open('models/{}/architecture.json'.format(VERSION)) as f:
-            model = model_from_json(f.read())
-        model.load_weights('models/{}/training_sessions/{}/weights.hdf5'.format(VERSION, int(SUBVERSION)-1))
+    from sys import argv
+    if len(argv) != 3:
+        print("Usage: python ff.py <version (like ff_grid_0)> <gridsearch_descriptor_string>")
+        exit(2)
+    VERSION = argv[1]
+    SUBVERSION = 0
+    subversion_details = argv[2]
+    ops = parse_subversion(subversion_details)
+    IMAGE_DIMENSION = ops['image_dimension']
+    BATCH_SIZE = ops['batch_size']
+    NUM_HIDDEN_LAYERS = ops['deepness']
+    HIDDEN_SIZE = ops['hidden_size']
+    #make a (image input) -> (NUM_HIDDEN_LAYERS * )(hidden) -> (classification output) network
+    model = Sequential()
+    model.add(Reshape((IMAGE_DIMENSION**2,), input_shape=(1, IMAGE_DIMENSION, IMAGE_DIMENSION)))
+    for _ in range(NUM_HIDDEN_LAYERS):
+        model.add(Dense(HIDDEN_SIZE))
+        model.add(Activation(ops['activation']))
+    model.add(Dense(NB_CLASSES))
+    model.add(Activation('softmax'))
     
-    optimizer=SGD()
-    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    optimizer=ops['optimizer']
+    if optimizer=='sgd' and 'learning_rate' in ops:
+        if 'momentum' in ops:
+            optimizer = SGD(lr=ops['learning_rate'], momentum=ops['momentum'])
+        else:
+            optimizer = SGD(lr=ops['learning_rate'])
+    model.compile(optimizer=optimizer, loss=ops['loss'], metrics=['accuracy'])
     
     image_loader = ImageDataGenerator(
         rotation_range=15,
