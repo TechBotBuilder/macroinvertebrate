6e5143abf2942025066ce88ea9a7dc9cec055c14
Maxwell Budd <techbotbuilder@gmail.com>
2016-10-31 01:02:08 -0400

    refactor

--- ff.py
+++ ff.py
@@ -1,28 +1,16 @@
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, Dropout
 from keras.layers import Reshape
 from keras.optimizers import SGD
+import resources as r
 
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import model_from_json
-
-import os
-import pickle
-
-NB_EPOCH = 100
+NB_EPOCH = 200
 SAMPLES_PER_EPOCH = 2048
-
 NUM_VALIDATION_SAMPLES = 113
 
 DATA_DIRECTORY = 'dog_data'
 
-#http://stackoverflow.com/a/36150375 was helpful here
-NB_CLASSES = len(next(os.walk('{}/training'.format(DATA_DIRECTORY)))[1])
-#^counts how many directories are in ^
-
-def log(message):
-    from datetime import datetime
-    print("[{}] {}".format(str(datetime.now())[:19], message))
+NB_CLASSES = r.num_classes(data_dir)
 
 def parse_subversion(subversion_descriptor):
     sv=subversion_descriptor
@@ -36,7 +24,10 @@ def parse_subversion(subversion_descriptor):
         #        + "Could not find {} in {}".format(letter, sv))
         #        exit(2)
         #else: return pos_letter
-    positions = {letter:whereis(letter) for letter in "HNIAL"}
+    letters = "HNIAL"
+    positions = {}
+    for letter_index in range(len(letters)):
+        positions[letters[letter_index]] = whereis(letters[letter_index], after=positions.get(letters[letter_index-1], 0))
     results = {}
     results['optimizer'] = sv[:positions['H']].lower()
     results['hidden_size'] = int(sv[positions['H']+1:positions['N']-1])
@@ -53,30 +44,37 @@ def parse_subversion(subversion_descriptor):
     if pos_m != -1: results['momentum'] = float(sv[pos_m+1:pos_m+4])
     pos_D = whereis('D', positions['L'])
     results['deepness'] = int(sv[pos_D+1:pos_D+2])
+    pos_d = whereis('d', pos_D, True)
+    if pos_d != -1: results['dropout'] = float(sv[pos_d+1:pos_d+4])
     return results
 
 if __name__ == "__main__":
-    log("Starting program...")
+    r.log("Starting program...")
     from sys import argv
     if len(argv) != 3:
         print("Usage: python ff.py <version (like ff_grid_0)> <gridsearch_descriptor_string>")
         exit(2)
     VERSION = argv[1]
-    SUBVERSION = 0
+    SUBVERSION = 1
     subversion_details = argv[2]
     ops = parse_subversion(subversion_details)
     IMAGE_DIMENSION = ops['image_dimension']
     BATCH_SIZE = ops['batch_size']
     NUM_HIDDEN_LAYERS = ops['deepness']
     HIDDEN_SIZE = ops['hidden_size']
-    #make a (image input) -> (NUM_HIDDEN_LAYERS * )(hidden) -> (classification output) network
-    model = Sequential()
-    model.add(Reshape((IMAGE_DIMENSION**2,), input_shape=(1, IMAGE_DIMENSION, IMAGE_DIMENSION)))
-    for _ in range(NUM_HIDDEN_LAYERS):
-        model.add(Dense(HIDDEN_SIZE))
-        model.add(Activation(ops['activation']))
-    model.add(Dense(NB_CLASSES))
-    model.add(Activation('softmax'))
+    if int(SUBVERSION) == 0:
+        #make a (image input) -> (NUM_HIDDEN_LAYERS * )(hidden) -> (classification output) network
+        model = Sequential()
+        model.add(Reshape((IMAGE_DIMENSION**2,), input_shape=(1, IMAGE_DIMENSION, IMAGE_DIMENSION)))
+        for _ in range(NUM_HIDDEN_LAYERS):
+            model.add(Dense(HIDDEN_SIZE))
+            model.add(Activation(ops['activation']))
+            if 'dropout' in ops:
+                model.add(Dropout(ops['dropout']))
+        model.add(Dense(NB_CLASSES))
+        model.add(Activation('softmax'))
+    else:
+        model = r.load(VERSION, int(SUBVERSION)-1)
     
     optimizer=ops['optimizer']
     if optimizer=='sgd' and 'learning_rate' in ops:
@@ -86,54 +84,17 @@ if __name__ == "__main__":
             optimizer = SGD(lr=ops['learning_rate'])
     model.compile(optimizer=optimizer, loss=ops['loss'], metrics=['accuracy'])
     
-    image_loader = ImageDataGenerator(
-        rotation_range=15,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        shear_range=0.1,
-        zoom_range=[3/2, 2/3],
-        fill_mode='constant',
-        cval=0
-        )
-    
-    options = {
-        'target_size': (IMAGE_DIMENSION, IMAGE_DIMENSION),
-        'color_mode': 'grayscale',
-        'class_mode': 'categorical',
-        'batch_size': BATCH_SIZE
-        }
-    
-    training_generator = image_loader.flow_from_directory('{}/training'.format(DATA_DIRECTORY), **options)
-    
-    validation_generator = image_loader.flow_from_directory('{}/validation'.format(DATA_DIRECTORY), **options)
+    training_generator, validation_generator = r.image_generators(IMAGE_DIMENSION, BATCH_SIZE, DATA_DIRECTORY)
     
     history = model.fit_generator(
         training_generator,
         samples_per_epoch = int(SAMPLES_PER_EPOCH / BATCH_SIZE) * BATCH_SIZE,
         nb_epoch = NB_EPOCH,
         validation_data = validation_generator,
-        nb_val_samples = NUM_VALIDATION_SAMPLES)
+        nb_val_samples = NUM_VALIDATION_SAMPLES,
+        verbose=0)
     
-    log("Training done. Saving everything...")
+    r.log("Training done. Saving everything...")
+    r.save(model, history, VERSION, SUBVERSION)
     
-    check_directories = (
-        'models/{}'.format(VERSION),
-        'models/{}/training_sessions'.format(VERSION),
-        'models/{}/training_sessions/{}'.format(VERSION, SUBVERSION)
-        )
-    #help from http://stackoverflow.com/a/273227
-    for checkdir in check_directories:
-        if not os.path.exists(checkdir):
-            os.makedirs(checkdir)
-    
-    model.save_weights('models/{}/training_sessions/{}/weights.hdf5'.format(VERSION, SUBVERSION))
-    
-    model_architecture = model.to_json()
-    with open('models/{}/architecture.json'.format(VERSION), 'w') as f:
-        f.write(model_architecture)
-    
-    
-    with open('models/{}/training_sessions/{}/history.txt'.format(VERSION, SUBVERSION), 'wb') as f:
-        pickle.dump(history, f)
-    
-    log("Done.")
+    r.log("Done.")
---
+++ resources.py
@@ -0,0 +1,71 @@
+#resources.py
+#use `import resources as r`
+
+
+def log(message):
+    from datetime import datetime
+    print("[{}] {}".format(str(datetime.now())[:19], message))
+
+def num_classes(DATA_DIRECTORY):
+    import os
+    #http://stackoverflow.com/a/36150375 was helpful here
+    return len(next(os.walk('{}/training'.format(DATA_DIRECTORY)))[1])
+
+def image_generators(IMAGE_DIMENSION, BATCH_SIZE, DATA_DIRECTORY):
+    from keras.preprocessing.image import ImageDataGenerator
+    image_loader = ImageDataGenerator(
+        rotation_range=25,
+        width_shift_range=0.05,
+        height_shift_range=0.05,
+        shear_range=0.05,
+        zoom_range=0.2,
+        fill_mode='constant',
+        cval=0
+        )
+    options = {
+        'target_size': (IMAGE_DIMENSION, IMAGE_DIMENSION),
+        'color_mode': 'grayscale',
+        'class_mode': 'categorical',
+        'batch_size': BATCH_SIZE
+        }
+    training_generator = image_loader.flow_from_directory('{}/training'.format(DATA_DIRECTORY), **options)
+    validation_generator = image_loader.flow_from_directory('{}/validation'.format(DATA_DIRECTORY), **options)
+    return training_generator, validation_generator
+
+def save_model(model, VERSION, SUBVERSION):
+    import os
+    check_directories = (
+        'models/{}'.format(VERSION),
+        'models/{}/training_sessions'.format(VERSION),
+        'models/{}/training_sessions/{}'.format(VERSION, SUBVERSION)
+        )
+    #help from http://stackoverflow.com/a/273227
+    for checkdir in check_directories:
+        if not os.path.exists(checkdir):
+            os.makedirs(checkdir)
+    model.save_weights('models/{}/training_sessions/{}/weights.hdf5'.format(VERSION, SUBVERSION))
+
+def save_architecture(model, VERSION):
+    model_architecture = model.to_json()
+    with open('models/{}/architecture.json'.format(VERSION), 'w') as f:
+        f.write(model_architecture)
+
+def save_history(history, VERSION, SUBVERSION):
+    import pickle
+    with open('models/{}/training_sessions/{}/history.txt'.format(VERSION, SUBVERSION), 'wb') as f:
+        pickle.dump(history, f)
+
+def save(model, history, VERSION, SUBVERSION):
+    save_model(model, VERSION, SUBVERSION)
+    save_architecture(model, VERSION)
+    save_history(history, VERSION, SUBVERSION)
+
+def load(VERSION, SUBVERSION):
+    from keras.models import model_from_json
+    with open('models/{}/architecture.json'.format(VERSION)) as f:
+        model = model_from_json(f.read())
+    model.load_weights('models/{}/training_sessions/{}/weights.hdf5'.format(VERSION, SUBVERSION))
+    return model
+
+
+
