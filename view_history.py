bdf18d467a984ef165a0b9b9a84a4df907379857
Maxwell Budd <techbotbuilder@gmail.com>
2016-12-21 22:15:57 -0500

    improve args, fix new/old history

--- view_history.py
+++ view_history.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from sys import argv
 
 #Command-line ops:
-# <version> <subversion>
+# <version> <subversion number> [-k (kernal size)] [-v (loss/acc)]
 
 def avg(values):
     return sum(values)/len(values)
@@ -15,27 +15,30 @@ def smoothed(values, kernal=5):
         newvalues[i]=avg([values[pt] for pt in surrounding])
     return newvalues
 
-assert 4>=len(argv)>=3
+assert 7>=len(argv)>=3
 version = argv[1]
 subversion = argv[2]
-if len(argv)==4:
-    smoothing=int(argv[3])
+if '-k' in argv:
+    smoothing=int(argv[argv.index('-k')+1])
 else:
     smoothing=5
+if '-v' in argv:
+    what = argv[argv.index('-v')+1]
+else:
+    what = 'acc'
 
 with open("models/{}/training_sessions/{}/history.txt".format(version,subversion), "rb") as f:
     res = pickle.load(f)
 
 #check new/old-style history
-if 'history' in res.__dict__:
-    res = res.history
+res = getattr(res, 'history', res) #returns res.history if history in res, else res.
 
 #see http://stackoverflow.com/a/4701285 - help to add legend
 fig = plt.figure()
 ax = plt.subplot(111)
 
-val, = plt.plot(smoothed(res['val_acc'], smoothing),'r',label="Validation")
-train, = plt.plot(smoothed(res['acc'], smoothing),'g',label="Training")
+val, = plt.plot(smoothed(res['val_'+what], smoothing),'r',label="Validation " + what.title())
+train, = plt.plot(smoothed(res[what], smoothing),'g',label="Training " + what.title())
 
 box = ax.get_position()
 ax.set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
